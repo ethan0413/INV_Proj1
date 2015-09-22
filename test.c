41c2e1d30e1be900637c289b9379189b263d6940
ethan0413 <ethan0413@live.com>
2015-09-22 15:35:21 +0800

    add debug functions

--- test.c
+++ test.c
@@ -1,6 +1,14 @@
-dddd
-aaaaaa
-aaaaaa
-aaaaaa
-ddd
-a1
+int function1()
+{
+	printf("test1\r\n");
+	return 0;
+}
+int function1()
+{
+	printf("test2\r\n");
+	return 0;
+}
+int main()
+{
+	return 0;
+}
